981f2eeda8b65fcd44c9cc344ef2ce5795ca0c9d
sfdcnaveen <pasupulatink@gmail.com>
2026-03-15 13:51:07 +0530

    Initial Commit

---
+++ NewNet/NewNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewNetApp.swift
+//  NewNet
+//
+//  Created by Naveen Kumar Pasupuleti on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct NewNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
